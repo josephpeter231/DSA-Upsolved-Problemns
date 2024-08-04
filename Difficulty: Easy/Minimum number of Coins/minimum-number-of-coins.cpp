17ef5fec2a1c37077f436fd176997b42efdbc5d5
Joseph Peter J <110652580+josephpeter231@users.noreply.github.com>
2024-08-04 20:11:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Minimum number of Coins/minimum-number-of-coins.cpp
+++ Difficulty: Easy/Minimum number of Coins/minimum-number-of-coins.cpp
@@ -9,32 +9,24 @@ using namespace std;
 
 class Solution{
 public:
-    vector<int> minPartition(int n)
+    vector<int> minPartition(int N)
     {
-        vector<int>v = {1,2,5,10,20,50,100,200,500,2000};
-        sort(v.begin(),v.end(),greater<int>());
         vector<int>res;
-        // vector
-        while(n>0){
-            for(int i=0;i<v.size();i++){
-                if(n>=v[i]){
-                    int x = n/v[i];
-                    for(int j=0;j<x;j++){
-                        res.push_back(v[i]);
-                    }
-                    n -= x*v[i];
-                    
-                    
-                    break;
+        vector<int>v = {2000,500,200,100,50,20,10,5,2,1};
+        for(auto it:v){
+            if(N>=it){
+                int x = N/it;
+                for(int j =0;j<x;j++){
+                    res.push_back(it);
                 }
+                N-=x*it;
+                
             }
         }
         return res;
-    
+        // code here
     }
-    
 };
-
 
 
 //{ Driver Code Starts.
