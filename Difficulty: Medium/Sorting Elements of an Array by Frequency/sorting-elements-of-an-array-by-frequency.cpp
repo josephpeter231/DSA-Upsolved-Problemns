fbfa11436a70d871b6fc2a5dcc589ed89aa3c607
Joseph Peter J <110652580+josephpeter231@users.noreply.github.com>
2024-08-28 08:45:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sorting Elements of an Array by Frequency/sorting-elements-of-an-array-by-frequency.cpp
+++ Difficulty: Medium/Sorting Elements of an Array by Frequency/sorting-elements-of-an-array-by-frequency.cpp
@@ -1,35 +1,60 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <algorithm>
+//{ Driver Code Starts
+#include <bits/stdc++.h>
 using namespace std;
 
+
+// } Driver Code Ends
+
+
+class Solution {
+public:
+    vector<int> sortByFreq(vector<int>& arr) {
+        unordered_map<int, int> freq_map;
+        for (int n : arr) freq_map[n]++;
+        
+        vector<pair<int, int>> freq_vec(freq_map.begin(), freq_map.end());
+        sort(freq_vec.begin(), freq_vec.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
+            if (a.second == b.second) return a.first < b.first;
+            return a.second > b.second;
+        });
+        
+        vector<int> result;
+        for (const auto& p : freq_vec) {
+            result.insert(result.end(), p.second, p.first);
+        }
+        
+        return result;
+    }
+};
+
+
+//{ Driver Code Starts.
+
 int main() {
+
     int t;
     cin >> t;
+    cin.ignore();
     while (t--) {
-        int n;
-        cin >> n;
-        vector<int> v(n);
-        unordered_map<int, int> up;
-        for (int i = 0; i < n; i++) {
-            cin >> v[i];
-            up[v[i]]++;
+
+        string input;
+        int num;
+        vector<int> arr;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            arr.push_back(num);
         }
-        
-        auto comp = [&up](int a, int b) {
-            if (up[a] == up[b]) {
-                return a < b; 
-            }
-            return up[a] > up[b]; 
-        };
-        
-        sort(v.begin(), v.end(), comp);
-        
-        for (auto it : v) {
-            cout << it << " ";
-        }
+        Solution obj;
+        vector<int> v;
+        v = obj.sortByFreq(arr);
+        for (int i : v)
+            cout << i << " ";
         cout << endl;
     }
+
     return 0;
 }
+
+// } Driver Code Ends
