8c56b6ca8b934bbce3b21b40818d9148a6db63e3
Joseph Peter J <110652580+josephpeter231@users.noreply.github.com>
2024-10-03 15:07:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Missing And Repeating/missing-and-repeating.py
+++ Difficulty: Medium/Missing And Repeating/missing-and-repeating.py
@@ -1,21 +1,16 @@
 #User function Template for python3
 
 class Solution:
-    def findTwoElement( self,arr): 
-        n=len(arr)
-        squared=0
-        summation=0
-        actual=(n*(n+1))//2
-        squaredactual= (n*(n+1)*(2*n + 1))//6
-        for i in range(n):
-            summation+=arr[i]
-            squared+=arr[i]**2
+    def findTwoElement(self, arr):
+        n = len(arr)
+        s = sum(arr)
+        s_set = sum(set(arr))
+        missing = n * (n + 1) // 2 - s_set
+        duplicate = s - s_set
+        return [duplicate, missing]
 
-        diff=actual-summation
-        missing=((squaredactual-squared)//diff + diff)//2
-        repeated=missing-diff
-        
-        return repeated,missing
+
+
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
@@ -24,7 +19,6 @@ if __name__ == '__main__':
 
     tc = int(input())
     while tc > 0:
-        n = int(input())
         arr = list(map(int, input().strip().split()))
         ob = Solution()
         ans = ob.findTwoElement(arr)
